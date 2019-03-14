fce6eac2c03f0b9843bf90b3f9355f63c271c449
Artefact <artefactgo@gmail.com>
2019-03-14 10:40:27 +0900

    AddTodoの仮実装

--- front_end/src/components/AddTodo.js
+++ front_end/src/components/AddTodo.js
@@ -3,11 +3,30 @@ import React from "react"
 class AddTodo extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      inputText: "",
+    }
   }
 
   render() {
-    return <h2>AddTodo</h2>
+    return (
+      <div>
+        <h2>AddTodo</h2>
+        <form>
+          <input value={this.state.inputText} />
+          <input onClick={this.handleOnClick} type="button" value="Add" />
+        </form>
+      </div>
+    )
+  }
+
+  handleOnChange = event => {
+    event.preventDefault()
+    this.setState({ inputText: event.target.value })
+  }
+
+  handleOnClick = event => {
+    this.setState({ inputText: "" })
   }
 }
 
