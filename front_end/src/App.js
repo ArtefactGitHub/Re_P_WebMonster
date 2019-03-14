b781e6c14ad190544d9735311776b8b4fad8cd90
Artefact <artefactgo@gmail.com>
2019-03-14 12:02:33 +0900

    AddTodoとの結合

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from "react"
+import React from "react"
 import "./App.css"
 
 import AddTodo from "./components/AddTodo"
 import TodoList from "./components/TodoList"
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <AddTodo />
+        <AddTodo OnSubmitTitle={this.handleOnSubmit} />
         <TodoList />
       </div>
     )
+  }
+
+  handleOnSubmit = title => {
+    this.setState({ todos: [...this.state.todos, { title: title }] })
   }
 }
 
