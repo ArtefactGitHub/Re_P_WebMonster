590e6c79043d2abaf90c679324c58bffaefc23ec
Artefact <artefactgo@gmail.com>
2019-03-14 11:57:42 +0900

    OnSubmit に修正

--- front_end/src/components/AddTodo.js
+++ front_end/src/components/AddTodo.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 class AddTodo extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       inputText: "",
     }
@@ -12,20 +12,21 @@ class AddTodo extends React.Component {
     return (
       <div>
         <h2>AddTodo</h2>
-        <form>
-          <input value={this.state.inputText} />
-          <input onClick={this.handleOnClick} type="button" value="Add" />
+        <form onSubmit={this.handleOnSubmit}>
+          <input onChange={this.handleOnChange} value={this.state.inputText} />
+          <input type="submit" value="Add" />
         </form>
       </div>
     )
   }
 
   handleOnChange = event => {
-    event.preventDefault()
     this.setState({ inputText: event.target.value })
   }
 
-  handleOnClick = event => {
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.props.OnSubmitTitle(this.state.inputText)
     this.setState({ inputText: "" })
   }
 }
