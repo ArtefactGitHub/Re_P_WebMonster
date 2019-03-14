f1d790d51a32e71e8e2cd80d8bef42542532dd6c
Artefact <artefactgo@gmail.com>
2019-03-14 12:10:14 +0900

    TodoListの実装

--- front_end/src/components/TodoList.js
+++ front_end/src/components/TodoList.js
@@ -1,13 +1,26 @@
 import React from "react"
 
 class TodoList extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {}
   }
 
   render() {
-    return <h2>TodoList</h2>
+    const list = this.props.todos.map(todo => {
+      return (
+        <li key={todo.id}>
+          {todo.id}: {todo.title}
+        </li>
+      )
+    })
+
+    return (
+      <div>
+        <h2>TodoList</h2>
+        <ul>{list}</ul>
+      </div>
+    )
   }
 }
 
