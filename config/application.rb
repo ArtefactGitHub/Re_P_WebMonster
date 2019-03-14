94764f2b1320b48bd73ab558f7560cf0af033f4f
Artefact <artefactgo@gmail.com>
2019-03-14 18:36:38 +0900

    日本語化の設定

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module RTPracticerailsreact
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     config.generators do |g|
       g.test_framework false
