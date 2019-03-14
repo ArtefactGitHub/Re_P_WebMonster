13d46075814213e18d47c00ff503069a92399b2e
Artefact <artefactgo@gmail.com>
2019-03-14 10:29:13 +0900

    コンポーネントを仮追加

---
+++ front_end/src/components/AddTodo.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class AddTodo extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return <h2>AddTodo</h2>
+  }
+}
+
+export default AddTodo
---
+++ front_end/src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class TodoList extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return <h2>TodoList</h2>
+  }
+}
+
+export default TodoList
