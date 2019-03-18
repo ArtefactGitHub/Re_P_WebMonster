20579738bbca0640b9f0ecd997cb2cda802a6888
Artefact <artefactgo@gmail.com>
2019-03-18 10:04:43 +0900

    非同期でアクションを発行するための redux-thunk を追加 yarn add redux-thunk

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
 import App from "./App"
+import thunk from "redux-thunk"
+
 import reducer from "./reducers"
 import * as serviceWorker from "./serviceWorker"
 import "./index.css"
 
-const store = createStore(reducer)
+const middlewares = [thunk]
+const store = createStore(reducer, applyMiddleware(...middlewares))
 
 ReactDOM.render(
   <Provider store={store}>
