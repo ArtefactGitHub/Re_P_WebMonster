f3ec449e669823164627c8fbb133d4a13ff89990
Artefact <artefactgo@gmail.com>
2019-03-18 11:23:03 +0900

    actionの生成を、ActionCreatorsを作成して移譲

--- front_end/src/containers/App.js
+++ front_end/src/containers/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { connect } from "react-redux"
-import axios from "axios"
 
 import AppPresenter from "../components/App"
-
-const HOST_URL = process.env.REACT_APP_BACKEND_URL
+import {
+  componentDidMount,
+  handleSubmitAddTodo,
+  handleClickDeleteTodo,
+} from "../actions/App"
 
 class App extends React.Component {
   componentDidMount() {
@@ -22,45 +24,6 @@ class App extends React.Component {
         />
       </div>
     )
-  }
-}
-
-const componentDidMount = () => {
-  return dispatch => {
-    axios
-      .get(`${HOST_URL}todos`)
-      .then(res => {
-        dispatch({ type: "LOAD_TODO", payload: { todos: res.data } })
-      })
-      .catch(data => {
-        console.log(data)
-      })
-  }
-}
-
-const handleSubmitAddTodo = todo => {
-  return dispatch => {
-    axios
-      .post(`${HOST_URL}todos`, todo)
-      .then(res => {
-        dispatch({ type: "ADD_TODO", payload: { todo: res.data } })
-      })
-      .catch(data => {
-        console.log(data)
-      })
-  }
-}
-
-const handleClickDeleteTodo = id => {
-  return dispatch => {
-    axios
-      .delete(`${HOST_URL}todos/${id}`)
-      .then(res => {
-        dispatch({ type: "DELETE_TODO", payload: { id: id } })
-      })
-      .catch(data => {
-        console.log(data)
-      })
   }
 }
 
