7a5b44f856cce97a2c51d258c0fdeca86c269c6b
Artefact <artefactgo@gmail.com>
2019-03-24 23:05:51 +0900

    モンスター作成APIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
-      resources :monsters, only: %i[index]
+      resources :monsters, only: %i[index create]
     end
   end
 end
