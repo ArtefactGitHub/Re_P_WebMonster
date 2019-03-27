bf6fef7ae658c5cafdd385f00d4c08177af0f8f3
Artefact <artefactgo@gmail.com>
2019-03-27 22:41:26 +0900

    redux-token-auth の reducer を root に統合

--- front_end/src/reducers/index.js
+++ front_end/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux"
+import { reduxTokenAuthReducer } from "redux-token-auth"
 
 import monsters from "./monsters"
 import {
@@ -7,6 +8,8 @@ import {
 } from "./create_monster"
 
 export default combineReducers({
+  reduxTokenAuth: reduxTokenAuthReducer,
+
   monsters,
   create_monster,
   createMonsterIsSubmitting,
