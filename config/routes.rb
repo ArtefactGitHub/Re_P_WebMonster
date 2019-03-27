408e7a5bfe4f05ef7bdc58ea4d9abb44637431fc
Artefact <artefactgo@gmail.com>
2019-03-27 19:26:20 +0900

    ユーザー登録のコントローラー、ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+
   namespace :api, format: 'json' do
     namespace :v1 do
       resources :monsters, only: %i[index create]
