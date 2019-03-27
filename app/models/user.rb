b6bf282e89126e941715ad900e2cf3312f77c5e2
Artefact <artefactgo@gmail.com>
2019-03-27 22:12:34 +0900

    Userモデルの devise 設定の修正 :trackable が有効だがマイグレーションに関連するカラムが含まれていないため実行時にエラーになる

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :trackable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
