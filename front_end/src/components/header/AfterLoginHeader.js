f177a140b956028fc2f3b5f8add1eadd9ef2f7ef
Artefact <artefactgo@gmail.com>
2019-03-30 11:39:47 +0900

    ヘッダーをNavbarの形に変更

--- front_end/src/components/header/AfterLoginHeader.js
+++ front_end/src/components/header/AfterLoginHeader.js
@@ -1,20 +1,48 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Navbar, Nav, NavDropdown, Button, Image } from "react-bootstrap"
 
 const AfterLoginHeader = () => {
   return (
     <div className="header">
-      <ul>
-        <li>
-          <Link to="/signOut">ログアウト</Link>
-        </li>
-        <li>
-          <Link to="/">モンスター一覧</Link>
-        </li>
-        <li>
-          <Link to="/mypage/monsters/new">モンスター作成</Link>
-        </li>
-      </ul>
+      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+        <Link to="/">
+          <Navbar.Brand>
+            <Image src="/logo.jpg" className="logo" />
+          </Navbar.Brand>
+        </Link>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/mypage/features" className="nav-link">
+              Features
+            </Link>
+
+            <NavDropdown title="Myモンスター" id="collasible-nav-dropdown">
+              <Link to="/mypage/monsters/new" className="dropdown-item">
+                作成
+              </Link>
+              <Link to="/mypage/monsters/#action_1" className="dropdown-item">
+                仮：アクション１
+              </Link>
+              <Link to="/mypage/monsters/#action_2" className="dropdown-item">
+                仮：アクション２
+              </Link>
+
+              <NavDropdown.Divider />
+              <Link to="/mypage/monsters/#action_3" className="dropdown-item">
+                仮：アクション３（セパレート付き）
+              </Link>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Link to="/signOut" className="link-to-logout">
+              <Button className="btn btn-default btn-raised">ログアウト</Button>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   )
 }
--- front_end/src/components/header/BeforeLoginHeader.js
+++ front_end/src/components/header/BeforeLoginHeader.js
@@ -1,23 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Navbar, Button, Image } from "react-bootstrap"
 
 const BeforeLoginHeader = () => {
   return (
     <div className="header">
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/signUp">ユーザー登録</Link>
-        </li>
-        <li>
-          <Link to="/signIn">ログイン</Link>
-        </li>
-        <li>
-          <Link to="/">モンスター一覧</Link>
-        </li>
-      </ul>
+      <Navbar bg="dark" variant="dark">
+        <Link to="/">
+          <Navbar.Brand>
+            <Image src="/logo.jpg" className="logo" />
+          </Navbar.Brand>
+        </Link>
+        <Link to="/signIn" className="link-to-login">
+          <Button className="btn btn-default btn-raised">ログイン</Button>
+        </Link>
+      </Navbar>
     </div>
   )
 }
